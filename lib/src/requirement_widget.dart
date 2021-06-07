737f8b11f56ac54aa4751b24756b7203728a442e
m-hamzashakeel <hamza.6.shakeel@gmail.com>
2021-06-07 18:31:53 +0500

    Package changes

--- lib/src/requirement_widget.dart
+++ lib/src/requirement_widget.dart
@@ -1,21 +1,40 @@
 import 'package:flutter/material.dart';
 
 class PassCheckRequirements extends StatelessWidget {
+
+  /// a `bool` value as check [required] field in case you want to `modify` the package
   final bool? passCheck;
+
+  /// requirement text [required] field in case you want to `modify` the package
   final String? requirementText;
+
+  /// IconData when requirement is completed
   final IconData? activeIcon;
+
+  /// IconData when requirement is not completed/inActive
   final IconData? inActiveIcon;
 
+  /// inActive color
   final Color? inActiveColor;
+
+  /// Active color
   final Color? activeColor;
 
   PassCheckRequirements({
     Key? key,
-    @required this.passCheck,
-    @required this.requirementText,
+    @required this.passCheck, /// [required parameters] in case you want to modify the package
+    @required this.requirementText, /// [required parameters] in case you want to modify the package
+
+    /// [default] value of in-active IconData
     this.inActiveIcon = Icons.check_circle_outline_rounded,
+
+    /// [default] value of active IconData
     this.activeIcon = Icons.check_circle_rounded,
+
+    /// [default] color of in-active field
     this.inActiveColor = Colors.grey,
+
+    /// [default] color of active field
     this.activeColor = Colors.blue,
   }) : super(key: key);
   @override
@@ -24,10 +43,14 @@ class PassCheckRequirements extends StatelessWidget {
       padding: const EdgeInsets.symmetric(vertical: 3.5),
       child: Row(
         children: [
+
+          /// requirement IconData based on check!
           passCheck!
               ? Icon(Icons.check_circle_rounded, color: activeColor)
               : Icon(Icons.check_circle_outline_rounded, color: inActiveColor),
           SizedBox(width: 8.0),
+
+          /// requirement text
           Text(
             requirementText!,
             style: TextStyle(
--- lib/src/validated_field.dart
+++ lib/src/validated_field.dart
@@ -2,38 +2,78 @@ import 'package:flutter/material.dart';
 import 'package:password_validated_field/src/requirement_widget.dart';
 
 class PasswordValidatedFields extends StatefulWidget {
-  final List<PassCheckRequirements>? moreRequirements;
+  
+  /// Password `validation` is given at the bottom which can be `modified` accordingly.
+  /// Full package can be modified easily
+  
+
+  /// Input decoration of Text field by default it is OutlineInputBorder
   final InputDecoration? inputDecoration;
+
+  /// textEditingController for the field
   final TextEditingController? textEditingController;
+
+  /// textInputAction for the field. By default its set to [done]
   final TextInputAction? textInputAction;
+
+  /// onEditComplete callBack for the field
   final void Function()? onEditComplete;
+
+  /// onFieldSubmitted callBack for the field
   final String Function(String)? onFieldSubmitted;
+
+  /// focusNode for the field
   final FocusNode? focusNode;
+
+  /// cursorColor
   final Color? cursorColor;
+
+  /// textStyle of the Text in field
   final TextStyle? textStyle;
-  // Password requirements
+
+  /// Password requirements attributes
+  /// iconData for the icons when requirement is completed
   final IconData? activeIcon;
+
+  /// iconData for the icons when the requirement is incomplete/inActive
   final IconData? inActiveIcon;
+
+  /// color of the text when requirement is completed
   final Color? activeRequirementColor;
+
+  /// color of the text when the requirement is not completed/inActive
   final Color? inActiveRequirementColor;
+
+  /// Constructor
   PasswordValidatedFields({
     Key? key,
-    this.moreRequirements,
+
+    /// [default inputDecoration]
     this.inputDecoration = const InputDecoration(
         hintText: "Enter password",
         prefixIcon: Icon(Icons.lock),
         border: OutlineInputBorder()),
     this.textEditingController,
+
+    /// [default textInputAction]
     this.textInputAction = TextInputAction.done,
     this.onEditComplete,
     this.onFieldSubmitted,
     this.focusNode,
     this.cursorColor,
     this.textStyle,
-    // Password requirements initialization
+
+    /// Password requirements initialization
+    /// [default inActiveIcon]
     this.inActiveIcon = Icons.check_circle_outline_rounded,
+
+    /// [default activeIcon]
     this.activeIcon = Icons.check_circle_rounded,
+
+    /// [default inActive Color]
     this.inActiveRequirementColor = Colors.grey,
+
+    /// [default active color]
     this.activeRequirementColor = Colors.blue,
   }) : super(key: key);
 
@@ -43,7 +83,6 @@ class PasswordValidatedFields extends StatefulWidget {
 }
 
 class _PasswordValidatedFieldsState extends State<PasswordValidatedFields> {
-  bool _moreRequirements = false;
   String _pass = "";
 
   @override
@@ -54,6 +93,9 @@ class _PasswordValidatedFieldsState extends State<PasswordValidatedFields> {
         mainAxisAlignment: MainAxisAlignment.center,
         mainAxisSize: MainAxisSize.min,
         children: [
+
+          /// [Password TextFormField]
+          /// Use `Form` to validate the field easily
           TextFormField(
             textInputAction: widget.textInputAction,
             controller: widget.textEditingController,
@@ -74,55 +116,60 @@ class _PasswordValidatedFieldsState extends State<PasswordValidatedFields> {
             validator: passwordValidation,
           ),
           SizedBox(height: 10.0),
+
+          /// [default requirements]
+          /// `1 Upper case` requirement
           PassCheckRequirements(
             passCheck: _pass.contains(RegExp(r'[A-Z]')),
             requirementText: "1 Uppercase [A-Z]",
           ),
+
+          /// `1 lowercase` requirement
           PassCheckRequirements(
             passCheck: _pass.contains(RegExp(r'[a-z]')),
             requirementText: "1 lowercase [a-z]",
           ),
+
+          /// `1 numeric value` requirement
           PassCheckRequirements(
             passCheck: _pass.contains(RegExp(r'[0-9]')),
             requirementText: "1 numeric value [0-9]",
           ),
+
+          /// `1 special character` requirement
           PassCheckRequirements(
             passCheck: _pass.contains(RegExp(r'[!@#$%^&*(),.?":{}|<>]')),
             requirementText: "1 special character [#, \$, % etc..]",
           ),
+
+          /// `6 character length` requirement
           PassCheckRequirements(
             passCheck: _pass.length >= 6,
             requirementText: "6 characters minimum",
           ),
-          _generateMoreRequirements()
         ],
       ),
     );
   }
 
+
+  /// [password validation]
+  /// 1 Uppercase
+  /// 1 lowercase
+  /// 1 numeric value
+  /// 1 special character
+  /// 6 length password
+  
+  /// In case you want to `modify` the requirements change the `RegExp` given below
   String? passwordValidation(String? value) {
     bool passValid =
-    RegExp(r'^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[!@#\$&*~]).{6,}$')
-        .hasMatch(value!);
+        RegExp(r'^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[!@#\$&*~]).{6,}$') /// [RegExp]
+            .hasMatch(value!);
     if (value.isEmpty) {
       return "Password cannot be emtpy!";
     } else if (!passValid) {
       return "Requirement(s) missing!";
     }
     return null;
-  }
-
-  bool _hasMoreRequirements() {
-    _moreRequirements = widget.moreRequirements == null ? false : true;
-    return _moreRequirements;
-  }
-
-  Widget _generateMoreRequirements() {
-    if (_hasMoreRequirements()) {
-      for (int i = 0; i < widget.moreRequirements!.length; i++) {
-        return widget.moreRequirements![i];
-      }
-    }
-    return Container();
   }
 }
